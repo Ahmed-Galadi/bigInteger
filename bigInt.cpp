73e4a322755cb2f91eb187c179ae31f93db5826c
Ahmed Galadi <galadiahmed99@gmail.com>
2025-09-22 14:08:26 +0100

    only shifting and tests left

--- bigInt.cpp
+++ bigInt.cpp
@@ -9,11 +9,74 @@ bigint::bigint(unsigned int num) {
 	data = ss.str();
 }
 
-bigint::bigint(const bigint &other) {
-	if (this != &other)
-		data = other.getData();
+bigint::bigint(const std::string &numStr) : data(numStr) {
+	this->removeZeros();
 }
 
+bigint::bigint(const bigint &other) : data(other.getData()) {
+	this->removeZeros();
+}
+
+bigint	&bigint::operator=(const bigint &other) {
+	if (this != &other)
+		data = other.getData();
+	this->removeZeros();
+	return (*this);
+}
+
+// remove leading zeros
+void	bigint::removeZeros() {
+	int zerosCount = 0;
+	for (; zerosCount < data.length() && data[zerosCount] == '0'; zerosCount++);
+	data.erase(0, zerosCount);
+}
+
+// addition
+bigint	bigint::operator+(const bigint &other) {
+	std::string output;
+	
+	int i = data.length() - 1;
+	int j = other.getData().length() -1;
+	int carry = 0;
+
+	while (i-- >= 0 || j-- >= 0 || !carry) {
+		int num1 = i >= 0 ? data[i] - '0' : 0;
+		int num2 = j >= 0 ? other.getData()[j] : 0;
+		int sum = num1 + num2 + carry;
+		output.push_back((sum % 10) + '0');
+		carry = sum / 10;
+	}
+	for (; i < output.length(); ++i)
+		output.push_back(output[i]);
+	return (bigint(output));
+}
+
+bigint	bigint::operator+(unsigned int number) {
+	return (*this + bigint(number));
+}
+
+bigint &bigint::operator+=(const bigint &other) {
+	data = (*this + other).getData();
+	return (*this);
+}
+
+bigint &bigint::operator+=(unsigned int number) {
+	data = (*this + number).getData();
+	return (*this);
+}
+
+bigint	&bigint::operator++() {
+	*this += 1;
+	return (*this);
+}
+
+bigint bigint::operator++(int) {
+	std::string dataHolder = data;
+	*this += 1;
+	return (bigint(data));
+}
+
+// shifting
 
 
 // printing
--- bigInt.hpp
+++ bigInt.hpp
@@ -6,16 +6,25 @@
 class bigint {
 	private:
 		std::string data;
+		
+		void	removeZeros(); 
 	public:
 		bigint();
 		bigint(unsigned int num);
+		bigint(const std::string &numStr);
 		bigint(const bigint &other);
 
-		// + addition
-		bigint	&operator+(const bigint &other);
+		bigint	&operator=(const bigint &other);
+	
+	// + addition
+		bigint	operator+(const bigint &other);
+		bigint	operator+(unsigned int number);
+
 		bigint	&operator+=(const bigint &other);
+		bigint	&operator+=(unsigned int number);
+
 		bigint	&operator++();
-		bigint	&operator++(int);
+		bigint	operator++(int);
 		// + comparison
 		bool	operator<(const bigint &other) const;
 		bool	operator<=(const bigint &other) const;
@@ -25,14 +34,14 @@ class bigint {
 		bool	operator!=(const bigint &other) const;
 		// + digit shift
 		//		- shift with object
-		bigint	&operator<<(const bigint &other);
+		bigint	operator<<(const bigint &other);
 		bigint  &operator<<=(const bigint &other);
-		bigint	&operator>>(const bigint &other);
+		bigint	operator>>(const bigint &other);
 		bigint	&operator>>=(const bigint &other);
 		//		- shift with int
-		bigint	&operator<<(int other);
+		bigint	operator<<(int other);
 		bigint  &operator<<=(int other);
-		bigint	&operator>>(int other);
+		bigint	operator>>(int other);
 		bigint	&operator>>=(int other);
 
 		std::string getData() const;
